090da9c18e0221f838e48a7e613b075434fbf6fb
Rodriguezbelen <beelu2.1997@gmail.com>
2025-01-28 22:31:30 -0300

    first commit

---
+++ mian.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('form');
+const contenedor = document.querySelector('.contenedor')
+
+let acumuladora = null
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // console.log(event.target.nombre.value)
+    // console.log(event.target.apellido.value)
+    // console.log(new formData(event.target))
+    const data = Object.fromEntries(new FormData(event.target));
+    // console.log(Object.fromEntries(new formData(event.target)))
+    // acumuladora =acumuladora + 'string nuevo';
+    // acumuladora += 'string nuevo';
+
+    // console.log(data);
+    // // Reset form
+    // event.target.reset();
+    // // Save data to local storage
+    // localStorage.setItem('userData', JSON.stringify(data));
+
+    acumuladora += `<div>
+                            <h2>${data.nombre}</h2>
+                            <h2 class="azul">${data.apellido}</h2>
+                            </div>`
+});
+
+contenedor.innerHTML = acumuladora
